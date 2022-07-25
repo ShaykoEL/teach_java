e9b7108413e2a5ed02b0ad2d374a775246f57928
KirillShabanov <shabanoffitstep@gmail.com>
2022-07-25 21:22:44 +0300

    new classwork

--- src/HW210722/Task3.java
+++ src/HW210722/Task3.java
@@ -1,20 +1,2 @@
-package HW210722;
-
-public class Task3 {
-    public static void main(String[] args) {
-
-        int array[] = new int[8];
-
-        for (int i = 0; i < array.length; i++) {
-            array [i] = (int) (Math.random() * 10) + 1;
-            System.out.print(Arrays.to[i] + " ");
-        }
-               for (int i = 0; i < array.length; i++) {
-            if (array[i] % 2 != 0) array[i] = 0;
-                   System.out.print(array[i] + " ");
-
-            }
-        }
-
-    }
-
+package HW210722;public class Task3 {
+}
