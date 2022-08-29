40e135cd14b6190932919ac97f938e154bf849d0
KirillShabanov <shabanoffitstep@gmail.com>
2022-08-29 21:08:46 +0300

    add classwork

---
+++ src/by/step/teach_java/classwork/classwork_290822_classes_constructor/task4/Calculator.java
@@ -0,0 +1,17 @@
+package by.step.teach_java.classwork.classwork_290822_classes_constructor.task4;
+
+public class Calculator {
+    public double plus (double x, double y){
+        return x+y;
+    }
+    public double minus (double x, double y){
+        return x-y;
+    }
+    public double del (double x, double y){
+        return x/y;
+    }
+    public double multiplication (double x, double y){
+        return x*y;
+    }
+}
+
