450fffa1a07a44ff7f679b80ba064afdc5a2550e
ShaykoEL <107205295+ShaykoEL@users.noreply.github.com>
2022-09-25 23:30:21 +0300

    add new fail

---
+++ src/by/step/teach_java/classwork/classwork_220922/Task1/Country.java
@@ -0,0 +1,34 @@
+package by.step.teach_java.classwork.classwork_220922.Task1;
+
+import java.util.Objects;
+
+public class Country {
+    private String name;
+    private String continent;
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setContinent(String continent) {
+       this.continent = continent;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Country country = (Country) o;
+        return Objects.equals(name, country.name) && Objects.equals(continent, country.continent);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, continent);
+    }
+
+    @Override
+    public String toString() {
+        return "Country " + name + ", continent= " + continent;
+    }
+}
---
+++ src/by/step/teach_java/classwork/classwork_220922/Task2/City.java
@@ -0,0 +1,37 @@
+package by.step.teach_java.classwork.classwork_220922.Task2;
+
+import java.util.Objects;
+
+public class City {
+    private String country;
+    private String name;
+    private int countOfPeople;
+
+    public void setCountry (String country){
+        this.country = country;
+    }
+    public void setName (String name){
+        this.name = name;
+    }
+    public void setCountOfPeople (int countOfPeople){
+        this.countOfPeople = countOfPeople;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        City city = (City) o;
+        return countOfPeople == city.countOfPeople && Objects.equals(country, city.country) && Objects.equals(name, city.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(country, name, countOfPeople);
+    }
+
+    @Override
+    public String toString() {
+        return "City: " + name + "country = "+ country +", countOfPeople= " + countOfPeople;
+    }
+}
