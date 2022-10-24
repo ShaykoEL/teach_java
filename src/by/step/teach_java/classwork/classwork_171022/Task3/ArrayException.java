4b7972477d2c4d6e2572a7847c134f9637ab2783
ShaykoEL <107205295+ShaykoEL@users.noreply.github.com>
2022-10-24 16:35:24 +0300

    new fail

---
+++ src/by/step/teach_java/classwork/classwork_171022/Task3/ArrayException.java
@@ -0,0 +1,29 @@
+package by.step.teach_java.classwork.classwork_171022.Task3;
+
+import java.util.Arrays;
+
+public class ArrayException {
+    public static void main(String[] args) {
+        int [] numbers = {167, 5, 655};
+        int [] numbers2 = {167, -2, 3};
+        ArrayException ar = new ArrayException();
+        ar.compareArr(numbers, 1,0, numbers2, 1,2);
+        ar.compareArr(numbers, 1, 4, numbers2, 1,2);
+        ar.compareArr(null, 1,4, numbers2, 1,2);
+
+    }
+    public void compareArr(int[] a, int aFromIndex, int aToIndex,
+                           int[] b, int bFromIndex, int bToIndex) {
+        try {
+            Arrays.compare(a, aFromIndex, aToIndex, b, bFromIndex, bToIndex);
+
+            }catch (IllegalArgumentException e) {
+            System.out.println("Hello from IllegalArgumentException");
+        }catch (ArrayIndexOutOfBoundsException e1) {
+            System.out.println("Hello from ArrayIndexOutOfBoundsException");
+        }catch (NullPointerException e2) {
+            System.out.println("Hello from NullPointerException");
+        }
+    }
+
+}
---
+++ src/by/step/teach_java/classwork/classwork_171022/Task4/Sum.java
@@ -0,0 +1,13 @@
+package by.step.teach_java.classwork.classwork_171022.Task4;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Sum {
+
+    public static void main(String[] args) throws Exception {
+
+
+    }
+}
