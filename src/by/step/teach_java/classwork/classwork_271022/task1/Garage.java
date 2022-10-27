180018c4b8bb1405eec1e3f2ea404e75fb40e9a5
KirillShabanov <shabanoffitstep@gmail.com>
2022-10-27 21:09:11 +0300

    add classwork Generik

---
+++ src/by/step/teach_java/classwork/classwork_271022/task1/Garage.java
@@ -0,0 +1,4 @@
+package by.step.teach_java.classwork.classwork_271022.task1;
+
+public class Garage {
+}
---
+++ src/by/step/teach_java/classwork/classwork_271022/task1/Transport.java
@@ -0,0 +1,6 @@
+package by.step.teach_java.classwork.classwork_271022.task1;
+
+public class Transport <T> {
+    private T item;
+
+}
