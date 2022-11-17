a7b675d305595fbab9219ff5f5016eb42b1db021
KirillShabanov <shabanoffitstep@gmail.com>
2022-11-17 21:15:22 +0300

    new fail

--- src/by/step/teach_java/homework/homework_pr3/Task3/StringBuilder.java
+++ src/by/step/teach_java/homework/homework_pr3/Task3/StringBuilder.java
@@ -1,4 +1,7 @@
 package by.step.teach_java.homework.homework_pr3.Task3;
 
 public class StringBuilder {
+    public static void main(String[] args) {
+        StringBuilder st = new StringBuilder();
+    }
 }
